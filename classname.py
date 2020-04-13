f8b2d5e2de975acf3930f37031c371fe641ae8d8
plumrx <plumrx@outlook.com>
2020-04-13 21:37:25 +0800

    refactor

--- classname.py
+++ classname.py
@@ -1,82 +1,55 @@
-# web-spider.py
-# <span itemprop="name">Flutter 学习笔记</span> 文章名称
+# classname.py
 
 import time
 import requests
 from bs4 import BeautifulSoup
 
 
-# 示例
-def testWeb(url, element):
-    # url = 'https://blog.mutoe.com/archives/'  # 目标地址
-    # 实例化固定套路??
-    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-
-    for course in soup.find_all(element):
-        # 打印该标签属性
-        print(course.text)
+# 创建文件
+def createFile(filename, filelist):
+    file = open(filename, 'w')
+    file.write('\n'.join(filelist))
 
 
-# testWeb('http://www.itest.info/courses','h4')
+# 转换时间
+def exchangeTime(mode):
+    t = time.time()
+    exchangedTime = time.strftime(mode, time.gmtime(t))
+    return exchangedTime
 
 
-'''
-1 文章名
-2 文章链接
-3 文章正文
-'''
-
-
-def blog(url):
-    res = requests.get(url)
-    res.encoding = 'utf-8'
-    soup = BeautifulSoup(res.text, 'html.parser')
-    # 用不用self的区别??
-    fileNameList = []
-    for span in soup.find_all('span', itemprop='name'):
-        print(span.text)
-        fileNameList.append(span.text)
-
-    file = open('spiderResuult-3.txt', 'a')
-    file.write('\n'.join(fileNameList))
-
-
-# blog('https://blog.mutoe.com/archives/')
-
-
-def getResponse(url):
-    res = requests.get(url)
-    res.encoding = 'utf-8'  # 转码
-    soup = BeautifulSoup(res.text, 'html.parser')
+# 获取目标地址内容
+def getRquests(url):
+    response = requests.get(url)
+    response.encoding = 'utf-8'  # 转码
+    res = response.text
+    soup = BeautifulSoup(res, 'html.parser')
     return soup
 
 
-def createFile(filename, list):
-    file = open(filename, 'w')
-    file.write('\n'.join(list))
+# <a href="/2020/flutter-study-notes/" class="post-title-link" itemprop="url">Flutter 学习笔记</a>
+# <div class="post-body" itemprop="articleBody" >
+
+# 获取文章名、文章地址、文章内容
+def getBlogInfo(url, filename):
+    blogInfoList = []
+
+    # 获取博客信息
+    response = getRquests(url)
+    for elements in response.find_all('a', class_='post-title-link'):
+        blogInfoDict = dict()
+        blogInfoDict['name'] = elements.text
+        blogInfoDict['link'] = url + elements.get('href')
+
+        # 获取每篇博文
+        blog = getRquests(url + elements.get('href'))
+        blogInfoDict['blog'] = blog.find('div', class_='post-body').prettify()
+
+        # 把字典信息加入到数组中
+        blogInfoList.append(blogInfoDict)
+        blogInfoStrList = map(str, blogInfoList)
+    # 创建文件名带时间的TXT文件
+    createFile(filename, blogInfoStrList)
 
 
-# https://blog.mutoe.com/
-def blogInformation(url):
-    t = time.time()
-    localTime = time.strftime('%Y%m%d%H%M', time.gmtime(t))
-    resText = getResponse(url)
-    # 文章信息列表
-    blog_information_list = []
-    for titlElement in resText.find_all('a', class_='post-title-link'):
-        blogDict = dict()  # 定义字典 {'name':'firstBlog','link':'http://...','context':'首先。。。。其次。。'}
-        blogDict['name'] = titlElement.text
-        blogDict['link'] = url + titlElement.get('href')
-        # 每篇文章
-        resContent = getResponse(blogDict['link'])
-        blogDict['content'] = resContent.find('div', class_='post-body').prettify()
-        blog_information_list.append(blogDict)
-
-    print(blog_information_list)
-    string_list = map(lambda info: str(info), blog_information_list)
-
-
-    createFile('spider' + localTime + '.txt', string_list)
-
-blogInformation('https://blog.mutoe.com')
-
+getBlogInfo('https://blog.mutoe.com', 'spider-web' + exchangeTime('%Y%m%d%H%M') + '.txt')
